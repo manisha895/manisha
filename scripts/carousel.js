f2b8c232ff46864937b1a6dc6884283e35b08a8c
rithiksachdev <rithiksachdev3@gmail.com>
2020-12-09 01:44:45 +0530

    commit

---
+++ scripts/carousel.js
@@ -0,0 +1,37 @@
+$(document).ready(function () {
+  var owl = $('.owl-carousel');
+
+  owl.owlCarousel({
+    autoplay: true,
+  lazyLoad: true,
+  loop: true,
+  margin: 20,
+   /*
+  animateOut: 'fadeOut',
+  animateIn: 'fadeIn',
+  */
+  responsiveClass: true,
+  autoHeight: true,
+  autoplayTimeout: 7000,
+  smartSpeed: 800,
+  nav: true,
+  responsive: {
+    0: {
+      items: 1
+    },
+
+    600: {
+      items: 3
+    },
+
+    1024: {
+      items: 4
+    },
+
+    1366: {
+      items: 4
+    }
+  }
+  });
+
+});
